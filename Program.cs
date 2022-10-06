928de2b50f1c86fb6ee52029d70512eaf315e98a
natalia <natka545@mail.ru>
2022-10-06 19:49:58 +0300

    Example10

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// программа которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+Console.WriteLine("введите трехзначное число n");
+int n = int.Parse(Console.ReadLine()!);
+int result = (n%100)/10;
+Console.WriteLine($"{result}");
